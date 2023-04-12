be9127277025d4a3ae9814aecc8c23cd35a305bc
Nikolay-ar <delonartt@gmail.com>
2023-04-12 08:52:55 +0500

    add model Recipe, Inggredient: add docker-compose (#17)

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
