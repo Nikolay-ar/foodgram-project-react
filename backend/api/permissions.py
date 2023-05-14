7428ca2594a86d2eeaf2eae09210425a355e27fd
Nikolay <delonartt@gmail.com>
2023-05-14 13:06:28 +0500

    2.6 в has_permission добавил запрет неавторизованному пользователю на post запрос.

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,6 +2,10 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
